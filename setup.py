2cffbf3ddb8f438a297bebf4c5ad07368471a382
Kaveh Bakhtiyari <kavehbc@yahoo.com>
2023-03-28 02:06:32 -0400

    updated pyproject.toml

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-import pathlib
-import pkg_resources
 import setuptools
 
 
@@ -9,22 +7,9 @@ with open("README.md", "r") as fh:
 with open('requirements.txt', "r") as f:
     install_requires = f.read().splitlines()
 
-# with pathlib.Path('requirements.txt').open() as requirements_txt:
-#     install_requires = [
-#         str(requirement)
-#         for requirement
-#         in pkg_resources.parse_requirements(requirements_txt)
-#     ]
-
 setuptools.setup(
-    name="streambox",
-    version="0.0.1",
-    author="Kaveh Bakhtiyari",
-    author_email="",
-    description="A package for generic data science utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kavehbc/streambox",
     packages=setuptools.find_packages(),
     install_requires=install_requires
 )
